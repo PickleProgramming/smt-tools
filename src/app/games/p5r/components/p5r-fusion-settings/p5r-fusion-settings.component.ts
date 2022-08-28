2fd4cc72a4a95e1c9eb36bda72c95a5b8873ca70
Matt Pickle <matthew.e.pickle@gmail.com>
2022-08-28 16:27:46 -0400

    Added p5 fusion-service and app-wide abstract service

---
+++ src/app/games/p5r/components/p5r-fusion-settings/p5r-fusion-settings.component.ts
@@ -0,0 +1,61 @@
+// import { Component, OnInit } from '@angular/core'
+// import { P5_COMPENDIUM } from '../../p5r-constants'
+// import { P5Compendium } from '../../p5r-data-models'
+
+// @Component({
+//   selector: 'app-p5r-fusion-settings',
+//   template: `
+//   <app-fusion-settings 
+//     [compendium] = compendium
+//     [packs] = packs
+//     [togglePack] = togglePack>
+//   </app-fusion-settings>
+//   `
+// })
+// export class P5FusionSettingsComponent implements OnInit {
+
+//   compendium: P5Compendium = P5_COMPENDIUM
+//   packs: string[] = [
+//     'Izanagi',
+//     'Orpheus',
+//     'Ariadne',
+//     'Asterius',
+//     'Thanatos',
+//     'Kaguya',
+//     'Magatsu-Izanagi',
+//     'Tsukiyomi',
+//     'Messiah',
+//     'Orpheus F',
+//     'Athena',
+//     'Izanagi-no-Okami',
+//     'Raoul'
+//   ]
+//   packDemons: { [pack: string]: string[] } = {
+//     'Izanagi': ['Izanagi', 'Izanagi Picaro'],
+//     'Orpheus': ['Orpheus', 'Orpherus Picaro'],
+//     'Ariadne': ['Ariadne', 'Ariadne Picaro'],
+//     'Asterius': ['Asterius', 'Asterius Picaro'],
+//     'Thanatos': ['Thanatos', 'Thanatos Picaro'],
+//     'Kaguya': ['Kaguya', 'Kaguya Picaro'],
+//     'Magatsu-Izanagi': ['Magatsu-Izanagi', 'Magatsu-Izanagi Picaro'],
+//     'Tsukiyomi': ['Tsukiyomi', 'Tsukiyomi Picaro'],
+//     'Messiah': ['Messiah', 'Messiah Picaro'],
+//     'Orpheus F': ['Orpheus F', 'Orpheus Picaro F'],
+//     'Athena': ['Athena', 'Athena Picaro'],
+//     'Izanagi-no-Okami': ['Izanagi-no-Okamki', 'Izanagi-no-Okami Picaro'],
+//     'Raol': ['Raol']
+//   }
+
+//   togglePack = (event: any): void => {
+//     let checked: boolean = event.srcElement.checked
+//     if(checked) {
+
+//     } else {
+
+//     }
+//   }
+
+//   constructor() { }
+
+//   ngOnInit(): void {  }
+// }
