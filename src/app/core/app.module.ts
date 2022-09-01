acd114cd7fd17b9a856e60f3dd018951aa569f7c
PickleProgramming <matthew.e.pickle@gmail.com>
2022-08-31 23:05:04 -0400

    testcases written for chain calculator

--- src/app/core/app.module.ts
+++ src/app/core/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-import { HeaderComponent } from './header/header.component'
-import { FooterComponent } from './footer/footer.component'
-import { GameWrapperComponent } from './game-wrapper/game-wrapper.component'
+import { HeaderComponent } from './components/header/header.component'
+import { FooterComponent } from './components/footer/footer.component'
+import { GameWrapperComponent } from './components/game-wrapper/game-wrapper.component'
 
 @NgModule({
   declarations: [
--- src/app/shared/components/fusion-chain/fusion-chain.component.ts
+++ src/app/shared/components/fusion-chain/fusion-chain.component.ts
@@ -48,4 +48,175 @@ export class FusionChainComponent implements OnInit {
         : this.skills!.filter(v =>
           v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
     )
+
+  //TODO: TESTING
+  testAll(): void {
+    for (let i = 0; i < 13; i++)
+      this.test(i)
+  }
+
+  testRange(cases: number[]): void {
+    for (let i of cases)
+      this.test(i)
+  }
+
+  test(testCase: number): void {
+    let skills: string[]
+    switch (testCase) {
+
+      //No Max Level, No Demon Name, Not Unique Skills, < 4 Skills
+      case 0:
+        skills = [
+          'Miracle Punch',
+          'Apt Pupil',
+          'Attack Master'
+        ]
+        console.log(this.chain.getChains(skills))
+        return
+      //Max Level, No Demon Name, Not Unique Skills, < 4 Skills
+      case 1:
+        skills = [
+          'Miracle Punch',
+          'Apt Pupil',
+          'Attack Master'
+        ]
+        this.chain.maxLevel = 20
+        console.log(this.chain.getChains(skills))
+        this.chain.maxLevel = 50
+        console.log(this.chain.getChains(skills))
+        this.chain.maxLevel = 99
+        return
+      //No Max Level, Demon Name, Not Unique Skills, < 4 Skills
+      case 2:
+        skills = [
+          'Miracle Punch',
+          'Apt Pupil',
+          'Attack Master'
+        ]
+        console.log(this.chain.getChains(skills), 'Quetzalcoatl')
+        return
+      //No Max Level, No Demon Name, Unique Skill, < 4 Skills
+      case 3:
+        skills = [
+          'Miracle Punch',
+          'Apt Pupil',
+          'Die For Me!'
+        ]
+        console.log(this.chain.getChains(skills))
+        return
+      //No Max Level, No Demon Name, Not Unique Skills, > 4 Skills
+      case 4:
+        skills = [
+          'Miracle Punch',
+          'Apt Pupil',
+          'Attack Master',
+          'Regenerate 3'
+        ]
+        console.log(this.chain.getChains(skills))
+        return
+      //Max Level, Demon Name, Not Unique Skills, > 4 Skills
+      case 5:
+        skills = [
+          'Miracle Punch',
+          'Apt Pupil',
+          'Attack Master'
+        ]
+        console.log(this.chain.getChains(skills), 'Ara Mitama')
+        this.chain.maxLevel = 20
+        console.log(this.chain.getChains(skills))
+        this.chain.maxLevel = 50
+        console.log(this.chain.getChains(skills))
+        this.chain.maxLevel = 99
+        return
+      //Max Level, No Demon Name, Unique Skills, < 4 Skills
+      case 6:
+        skills = [
+          'Miracle Punch',
+          'Apt Pupil',
+          'Die For Me!'
+        ]
+        console.log(this.chain.getChains(skills), 'Alice')
+        this.chain.maxLevel = 20
+        console.log(this.chain.getChains(skills))
+        this.chain.maxLevel = 50
+        console.log(this.chain.getChains(skills))
+        this.chain.maxLevel = 99
+        return
+      //Max Level, No Demon Name, Not Unique Skills, = 4 Skills
+      case 7:
+        skills = [
+          'Miracle Punch',
+          'Apt Pupil',
+          'Attack Master',
+          'Growth 3'
+        ]
+        console.log(this.chain.getChains(skills))
+        this.chain.maxLevel = 20
+        console.log(this.chain.getChains(skills))
+        this.chain.maxLevel = 50
+        console.log(this.chain.getChains(skills))
+        this.chain.maxLevel = 99
+        return
+      //Max Level, Demon Name, Unique Skills, < 4 Skills
+      case 8:
+        skills = [
+          'Miracle Punch',
+          'Apt Pupil',
+          'Die For Me!'
+        ]
+        console.log(this.chain.getChains(skills), 'Alice')
+        this.chain.maxLevel = 20
+        console.log(this.chain.getChains(skills), 'Alice')
+        this.chain.maxLevel = 50
+        console.log(this.chain.getChains(skills), 'Alice')
+        this.chain.maxLevel = 99
+        return
+      //Max Level, Demon Name, Unique Skills, > 4 Skills
+      case 9:
+        skills = [
+          'Miracle Punch',
+          'Apt Pupil',
+          'Die For Me!'
+        ]
+        console.log(this.chain.getChains(skills), 'Alice')
+        this.chain.maxLevel = 20
+        console.log(this.chain.getChains(skills), 'Alice')
+        this.chain.maxLevel = 50
+        console.log(this.chain.getChains(skills), 'Alice')
+        this.chain.maxLevel = 99
+        return
+      //No Max Level, Demon Name, Unique Skills, < 4 Skills
+      case 10:
+        skills = [
+          'Miracle Punch',
+          'Apt Pupil',
+          'Die For Me!'
+        ]
+        console.log(this.chain.getChains(skills), 'Alice')
+        return
+      //No Max Level, Demon Name, Unique Skills, > 4 Skills
+      case 11:
+        skills = [
+          'Miracle Punch',
+          'Apt Pupil',
+          'Attack Master',
+          'Die For Me!'
+        ]
+        console.log(this.chain.getChains(skills), 'Alice')
+        return
+      //No Max Level, No Demon Name, Unique Skills, > 4 Skills
+      case 12:
+        skills = [
+          'Miracle Punch',
+          'Apt Pupil',
+          'Attack Master',
+          'Die For Me!'
+        ]
+        console.log(this.chain.getChains(skills))
+        return
+      default:
+        console.log('Please use a proper test number')
+        return
+    }
+  }
 }
