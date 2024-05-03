8d4b3e8b465bdab7411cc9a9f314dac5ae5ac268
PickleProgramming <matthew.e.pickle@gmail.com>
2024-05-02 22:11:38 -0400

    remove GameWrapperComponent becasue apparently it was no longer in use

--- src/app/core/app.module.ts
+++ src/app/core/app.module.ts
@@ -4,17 +4,11 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './components/header/header.component'
 import { FooterComponent } from './components/footer/footer.component'
-import { GameWrapperComponent } from './components/game-wrapper/game-wrapper.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatCardModule } from '@angular/material/card'
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		HeaderComponent,
-		FooterComponent,
-		GameWrapperComponent,
-	],
+	declarations: [AppComponent, HeaderComponent, FooterComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
