15eb768d4cf28cbf32da23c3578b1754be358382
PickleProgramming <matthew.e.pickle@gmail.com>
2024-05-03 16:42:01 -0400

    adds p5-persona-list-component
    updates necessary p5r modules to facilitate the addition of the
    p5-persona-list-component

--- src/app/games/p5r/p5r-routing.module.ts
+++ src/app/games/p5r/p5r-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 
+import { P5RPersonaListComponent } from './components/p5r-persona-list.component'
 import { P5RPersonaEntryComponent } from './components/p5r-persona-entry.component'
+import { P5HeaderComponent } from '@p5/components/p5-game-header.component'
 
 const routes: Routes = [
-	{ path: '', redirectTo: 'personas', pathMatch: 'full' },
 	{
-		path: 'personas',
-		component: P5RPersonaEntryComponent,
-		data: { gameName: 'p5r' },
+		path: '',
+		component: P5HeaderComponent,
+		children: [{ path: 'personas', component: P5RPersonaListComponent }],
 	},
 ]
 
--- src/app/games/p5r/p5r.module.ts
+++ src/app/games/p5r/p5r.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { RouterModule } from '@angular/router'
+import { SharedModule } from '@shared/shared.module'
+
+import { P5RPersonaListComponent } from './components/p5r-persona-list.component'
 import { P5RPersonaEntryComponent } from './components/p5r-persona-entry.component'
 import { P5RRoutingModule } from './p5r-routing.module'
 
 @NgModule({
-	declarations: [P5RPersonaEntryComponent],
-	imports: [CommonModule, P5RRoutingModule],
+	declarations: [P5RPersonaListComponent, P5RPersonaEntryComponent],
+	imports: [P5RRoutingModule, CommonModule, SharedModule, RouterModule],
 })
 export class P5RModule {}
