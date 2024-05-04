1bc3ec872b87e68d30cc73572f400b8afb18f7e3
PickleProgramming <matthew.e.pickle@gmail.com>
2024-05-03 22:01:26 -0400

    moves header.component template to seperate html file

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -6,19 +6,7 @@ import GAME_MODELS from 'src/assets/game-models.json'
 @Component({
 	selector: 'app-header',
 	styleUrls: ['./header.component.scss'],
-	template: `
-		<ul class="smt-navbar">
-			<li
-				*ngFor="let game of gameViews | keyvalue"
-				class="{{ game.key }}"
-			>
-				<a routerLink="{{ game.key }}" routerLinkActive="active">
-					<img src="{{ game.value.logo }}" />
-				</a>
-			</li>
-		</ul>
-		<router-outlet></router-outlet>
-	`,
+	templateUrl: 'header.component.html',
 })
 export class HeaderComponent implements OnInit {
 	gameViews: { [game: string]: GameView } = GAME_MODELS
