1165e210b8468d8b8a6cfb52c45ec3c00922c549
PickleProgramming <matthew.e.pickle@gmail.com>
2024-05-03 23:08:56 -0400

    turned p5r into placeholder to facilitate rework

--- src/app/games/p5r/p5r-routing.module.ts
+++ src/app/games/p5r/p5r-routing.module.ts
@@ -1,29 +1,14 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 
-import { P5HeaderComponent } from '@p5/components/p5-game-header.component'
-import { P5RPersonaListComponent } from './components/p5r-persona-list.component'
 import { P5RPersonaEntryComponent } from './components/p5r-persona-entry.component'
-import { P5RSkillListComponent } from './components/p5r-skill-list.component'
-import { P5RFusionTableComponent } from './components/p5r-fusion-table.component'
-import { P5RFusionChainComponent } from './components/p5r-fusion-chain.component'
-import { P5RSettingsComponent } from './components/p5r-settings.component'
 
 const routes: Routes = [
+	{ path: '', redirectTo: 'personas', pathMatch: 'full' },
 	{
-		path: '',
-		component: P5HeaderComponent,
-		children: [
-			{ path: 'personas', component: P5RPersonaListComponent },
-			{
-				path: 'personas/:demonName',
-				component: P5RPersonaEntryComponent,
-			},
-			{ path: 'skills', component: P5RSkillListComponent },
-			{ path: 'fusion-table', component: P5RFusionTableComponent },
-			{ path: 'fusion-chain', component: P5RFusionChainComponent },
-			{ path: 'settings', component: P5RSettingsComponent },
-		],
+		path: 'personas',
+		component: P5RPersonaEntryComponent,
+		data: { gameName: 'p5r' },
 	},
 ]
 
