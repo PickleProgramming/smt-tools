fb69bbaad7d7115468b5687a8da363fbda3ec024
PickleProgramming <matthew.e.pickle@gmail.com>
2024-05-06 17:41:48 -0400

    rearranges imports in shared.module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,27 +1,29 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { DemonListComponent } from './components/demon-list/demon-list.component'
 import { RouterModule } from '@angular/router'
-import { SkillListComponent } from './components/skill-list/skill-list.component'
-import { NormalFusionTableComponent } from './components/normal-fusion-table/normal-fusion-table.component'
-import { ElementFusionTableComponent } from './components/element-fusion-table/element-fusion-table.component'
-import { SettingsComponent } from './components/settings/settings.component'
-import { ShortenPipe } from './pipes/shorten.pipe'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { DemonEntryComponent } from './components/demon-entry/demon-entry.component'
-import { FusionChainComponent } from './components/fusion-chain/fusion-chain.component'
+
 import { MatExpansionModule } from '@angular/material/expansion'
 import { MatAutocompleteModule } from '@angular/material/autocomplete'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
-import { DataTablesModule } from 'angular-datatables'
 import { MatTableModule } from '@angular/material/table'
 import { MatSortModule } from '@angular/material/sort'
 import { MatCheckboxModule } from '@angular/material/checkbox'
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 import { MatSelectModule } from '@angular/material/select'
+import { DataTablesModule } from 'angular-datatables'
+
+import { DemonListComponent } from './components/demon-list/demon-list.component'
+import { SkillListComponent } from './components/skill-list/skill-list.component'
+import { NormalFusionTableComponent } from './components/normal-fusion-table/normal-fusion-table.component'
+import { ElementFusionTableComponent } from './components/element-fusion-table/element-fusion-table.component'
+import { SettingsComponent } from './components/settings/settings.component'
+import { DemonEntryComponent } from './components/demon-entry/demon-entry.component'
+import { FusionChainComponent } from './components/fusion-chain/fusion-chain.component'
+import { ShortenPipe } from './pipes/shorten.pipe'
 
 @NgModule({
 	declarations: [
@@ -45,12 +47,12 @@ import { MatSelectModule } from '@angular/material/select'
 		MatInputModule,
 		MatButtonModule,
 		MatCardModule,
-		DataTablesModule,
 		MatTableModule,
 		MatSortModule,
 		MatCheckboxModule,
 		MatProgressSpinnerModule,
 		MatSelectModule,
+		DataTablesModule,
 	],
 	exports: [
 		DemonListComponent,
