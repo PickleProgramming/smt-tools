26342011385fa7f29bcaa80baa96e50de6851a49
PickleProgramming <matthew.e.pickle@gmail.com>
2024-05-06 19:37:52 -0400

    adds styling to sidenav

--- src/app/core/components/app/app.component.ts
+++ src/app/core/components/app/app.component.ts
@@ -16,21 +16,21 @@ export class AppComponent {
 
 	buildGames() {
 		let abbrvs: { abbrv: string; color: string }[] = [
-			{ abbrv: 'p5', color: '#000000' },
-			{ abbrv: 'p5r', color: '#000000' },
-			{ abbrv: 'p4g', color: '#000000' },
-			{ abbrv: 'smt4', color: '#000000' },
+			{ abbrv: 'p5', color: '#c10b0a' },
+			{ abbrv: 'p5r', color: '#c10b0a' },
+			{ abbrv: 'p4g', color: '#fcc203' },
+			{ abbrv: 'smt4', color: '#520c85' },
 			{ abbrv: 'smt3hd', color: '#000000' },
-			{ abbrv: 'smtsjr', color: '#000000' },
-			{ abbrv: 'pq2', color: '#000000' },
-			{ abbrv: 'p3p', color: '#000000' },
+			{ abbrv: 'smtsjr', color: '#095709' },
+			{ abbrv: 'pq2', color: '#9105f5' },
+			{ abbrv: 'p3p', color: '#ffffff' },
 		]
 		let logos = 'assets/img/game-logos/'
 		for (let game of abbrvs) {
 			this.links.push({
 				url: game.abbrv,
 				img: logos + game.abbrv + '.png',
-				color: '#000000',
+				color: game.color,
 			})
 		}
 	}
