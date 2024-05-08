f7dd089fcf29c7c5262153b34f7e2d5f2645fea3
PickleProgramming <matthew.e.pickle@gmail.com>
2024-05-08 18:30:52 -0400

    styles p5-skill-list component

--- src/app/games/p5/components/p5-skill-list/p5-skill-list.component.ts
+++ src/app/games/p5/components/p5-skill-list/p5-skill-list.component.ts
@@ -8,6 +8,7 @@ import { P5_COMPENDIUM, P5_TABLE_CONFIG } from '@shared/constants'
 	selector: 'app-p5-skill-list',
 	template: ` <app-skill-list [skills]="skills" [tableConfig]="tableConfig">
 	</app-skill-list>`,
+	styleUrl: './p5-skill-list.component.scss',
 })
 export class P5SkillListComponent implements OnInit {
 	skills: { [name: string]: Skill } = P5_COMPENDIUM.skills
