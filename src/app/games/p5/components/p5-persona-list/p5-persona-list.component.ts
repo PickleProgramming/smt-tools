3bb21a4a734fba5c2329c28a34952b768cf0196b
PickleProgramming <matthew.e.pickle@gmail.com>
2024-05-08 13:21:36 -0400

    shortens some race names in p5-persona-list

--- src/app/games/p5/components/p5-persona-list/p5-persona-list.component.ts
+++ src/app/games/p5/components/p5-persona-list/p5-persona-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 
+import _ from 'lodash'
+
 import { P5TableConfig } from '@p5/types/p5-table-config'
 import { Demon } from '@shared/types/smt-tools.types'
 
@@ -14,9 +16,37 @@ import { P5_COMPENDIUM, P5_TABLE_CONFIG } from '@shared/constants'
 })
 export class P5PersonaListComponent implements OnInit {
 	tableConfig: P5TableConfig = P5_TABLE_CONFIG
-	demons: { [name: string]: Demon } = P5_COMPENDIUM.demons
+	demons: { [name: string]: Demon } = this.shortenRacesDemons(
+		P5_COMPENDIUM.demons
+	)
 
 	constructor() {}
+
+	/* copies the compendium's demons, recreates the list, and shortens the 
+		race string for any demon of the race Judgement, Hieorphant, or 
+		Temperance. This is done to reflect the race column in the compendium
+		in the actual P5 game
+	 */
+	shortenRacesDemons(demons: { [name: string]: Demon }): {
+		[name: string]: Demon
+	} {
+		let ret = _.cloneDeep(demons)
+
+		for (let demonName in ret) {
+			let demon = ret[demonName]
+			if (demon.race == 'Judgement') {
+				demon.race = 'Judge.'
+			}
+			if (demon.race == 'Hierophant') {
+				demon.race = 'Hiero.'
+			}
+			if (demon.race == 'Temperance') {
+				demon.race = 'Temper.'
+			}
+		}
+
+		return ret
+	}
 
 	ngOnInit(): void {}
 }
