fb3c07d2b3987f26f27706b49ece69822346a174
PickleProgramming <matthew.e.pickle@gmail.com>
2024-05-13 14:13:22 -0400

    updates documentation in smt-tools.types.ts

--- src/app/shared/types/smt-tools.types.ts
+++ src/app/shared/types/smt-tools.types.ts
@@ -1,5 +1,7 @@
-/* Object that stores the information necessary to populate Demon List tables
-    and calculate fusions  */
+/**
+ * Interface that stores the information necessary to populate Demon List tables
+ * and calculate fusions
+ */
 export interface Demon {
 	race: string
 	level: number
@@ -16,9 +18,12 @@ export interface Demon {
 	align?: string
 }
 
-/* Object that stores the info necessary to populate Skill List tables
-    and calculate fusion chaines
-   @learnedBy can only be calculated with a demon list */
+/**
+ * Interface that stores the info necessary to populate Skill List tables and
+ * calculate fusion chaines
+ *
+ * @property learnedBy Can only be calculated with a demon list
+ */
 export interface Skill {
 	element: string
 	effect: string
@@ -35,29 +40,51 @@ export interface Skill {
 	rank?: number
 }
 
-/* Object that keeps track of the demon races in a game as well as the table
-    that dictates their fusion */
+/**
+ * Interface that keeps track of the demon races in a game as well as the table
+ * that dictates their fusion
+ */
 export interface FusionTable {
 	races: string[]
 	table: string[][]
 }
 
-/* Object that keeps track of the elemental races in a game as well as the table
-    that dictates their fusions with demons */
+/**
+ * Interface that keeps track of the elemental races in a game as well as the
+ * table that dictates their fusions with demons
+ */
 export interface ElementTable {
 	elems: string[]
 	races: string[]
 	table: number[][]
 }
 
+/**
+ * Interface that stores a single fusion in the form of the fusion sources, the
+ * resultant demon, and optionally the estimated cost of the fusion
+ */
 export interface Recipe {
 	sources: string[]
 	result: string
 	cost?: number
 }
 
-/* Object that stores a chain of fusions to create a desired results, as well 
-	as store directions to follow to create the result*/
+/**
+ * Interface that stores a chain of fusions to create a desired results, as well
+ * as store directions to follow to create the result
+ *
+ * @property steps A list of recipes that will result in the resultant demon
+ * @property cost Estimated cost of fusing all the demons
+ * @property inherittedSkills A list parallel to steps that specificies what
+ *   skills should be inheritted at each step
+ * @property innates Skills that the resultant demon will be able to learn by
+ *   leveling up
+ * @property level The default level of the resultant demon
+ * @property result The name of the resultant demon
+ * @property directions A list of strings that tell the user how to follow the
+ *   fusion chain in plain english. This is what is meant to be displayed on the
+ *   webpage
+ */
 export interface FusionChain {
 	steps: Recipe[]
 	cost: number
@@ -68,45 +95,22 @@ export interface FusionChain {
 	directions: string[]
 }
 
-/*  Object that contains a series of fusion chains that are found based on 
-	the user input. Sent to the DOM as a message via rxjs. Also contains a 
-	series of reasons that certain fusions were impossible. If the results
-	are empty, but the errors are not, the fusion the user asked for was 
-	impossible. */
+/**
+ * Interface that contains a series of fusion chains that are found based on the
+ * user input. Sent to the DOM as a message via rxjs. Also contains a series of
+ * reasons that certain fusions were impossible. If the results are empty, but
+ * the errors are not, the fusion the user asked for was impossible.
+ */
 export interface ResultsMessage {
 	results: FusionChain[] | null
 	combo: number | null
 	error: string | null
 }
 
-/* User input data taken from the demon form*/
+/** User input data taken from the demon form */
 export interface InputChainData {
 	demonName: string | null
 	level: number | null
 	inputSkills: string[]
 	deep: boolean
-}
-
-/* Interface to store the different values for each inividual game
-	@property mainList - whether the game uses Demons or Personas
-	@property otherLinks - all the links the game uses apart from the universal ones
-	@property hasSettings - wether the game hase a settings link or not */
-export interface GameView {
-	game: string
-	logo: string
-	colors: Colors
-	font: string
-	mainList: string
-	otherLinks: Link[]
-}
-
-export interface Colors {
-	primary: string
-	secondary: string
-	text: string
-}
-
-export interface Link {
-	title: string
-	url: string
 }
