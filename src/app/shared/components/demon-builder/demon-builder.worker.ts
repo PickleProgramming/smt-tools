f1426526a8c3efc79b179a610cf6037579b70a5f
PickleProgramming <matthew.e.pickle@gmail.com>
2024-05-17 20:03:55 -0400

    remove work() function
    Alright I deleted my ~/.vscode directory and that seemed to fix it.

--- src/app/shared/components/demon-builder/demon-builder.worker.ts
+++ src/app/shared/components/demon-builder/demon-builder.worker.ts
@@ -1,55 +1,46 @@
-import { P5_DEMON_BUILDER } from '@shared/constants'
+import { P5_DEMON_BUILDER } from "@shared/constants";
 import {
-	ResultsMessage,
-	BuildRecipe,
-	InputChainData,
-} from '@shared/types/smt-tools.types'
-import { DoWorkUnit, runWorker } from 'observable-webworker'
-import { Observable, ReplaySubject, Subject, Subscription } from 'rxjs'
+  ResultsMessage,
+  BuildRecipe,
+  InputChainData,
+} from "@shared/types/smt-tools.types";
+import { DoWorkUnit, runWorker } from "observable-webworker";
+import { Observable, ReplaySubject, Subject, Subscription } from "rxjs";
 
 /* Class to run the fusion calculator in a web worker utilizes the 
     observable-worker library to make things much easier to deal with 
     https://github.com/cloudnc/observable-webworker*/
 export class DemonBuilderWorker
-	implements DoWorkUnit<InputChainData, ResultsMessage>
+  implements DoWorkUnit<InputChainData, ResultsMessage>
 {
-	//TODO: TypeScript thinks I need this even though I don't
-	work(
-		input: InputChainData
-	): Observable<ResultsMessage> | PromiseLike<ResultsMessage> {
-		throw new Error('Method not implemented.')
-	}
+  chains: BuildRecipe[] = [];
+  demonBuilder = P5_DEMON_BUILDER;
 
-	chains: BuildRecipe[] = []
-	demonBuilder = P5_DEMON_BUILDER
+  public workUnit(input: InputChainData): Observable<ResultsMessage> {
+    const output$: Subject<ResultsMessage> = new ReplaySubject(Infinity);
+    const sub = this.demonBuilder.resultMessageObservable.subscribe((data) => {
+      output$.next({
+        results: data.results,
+        fusionCounter: data.fusionCounter,
+        error: data.error,
+      });
+    });
+    this.calculate(input);
+    sub.unsubscribe();
+    return output$;
+  }
 
-	public workUnit(input: InputChainData): Observable<ResultsMessage> {
-		const output$: Subject<ResultsMessage> = new ReplaySubject(Infinity)
-		const sub = this.demonBuilder.resultMessageObservable.subscribe(
-			(data) => {
-				output$.next({
-					results: data.results,
-					fusionCounter: data.fusionCounter,
-					error: data.error,
-				})
-			}
-		)
-		this.calculate(input)
-		sub.unsubscribe()
-		return output$
-	}
-
-	private calculate(inputData: InputChainData): Observable<ResultsMessage> {
-		this.demonBuilder.recurDepth = inputData.recurDepth
-		if (inputData.level) this.demonBuilder.maxLevel = inputData.level
-		if (inputData.demonName) {
-			return this.demonBuilder.getFusionChains(
-				inputData.inputSkills,
-				inputData.demonName
-			)
-		}
-		return this.demonBuilder.getFusionChains(inputData.inputSkills)
-	}
+  private calculate(inputData: InputChainData): Observable<ResultsMessage> {
+    this.demonBuilder.recurDepth = inputData.recurDepth;
+    if (inputData.level) this.demonBuilder.maxLevel = inputData.level;
+    if (inputData.demonName) {
+      return this.demonBuilder.getFusionChains(
+        inputData.inputSkills,
+        inputData.demonName
+      );
+    }
+    return this.demonBuilder.getFusionChains(inputData.inputSkills);
+  }
 }
 
-runWorker(DemonBuilderWorker)
+runWorker(DemonBuilderWorker);
