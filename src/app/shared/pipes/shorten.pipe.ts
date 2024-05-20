7c145f3fea682800ca1be1f4ed0f9b465f82f820
PickleProgramming <matthew.e.pickle@gmail.com>
2024-05-20 12:44:00 -0400

    document shorten.pipe

--- src/app/shared/pipes/shorten.pipe.ts
+++ src/app/shared/pipes/shorten.pipe.ts
@@ -1,13 +1,28 @@
 import { Pipe, PipeTransform } from '@angular/core'
 import SHORTEN from '@shared/pipes/shorten.json'
 
+/**
+ * Used to get the specific three letter code that will represent a word in a
+ * table pulled from a JSON that I wrote. EX. Judgement -> Jud Priestess -> Prs
+ *
+ * @class ShortenPipe
+ * @typedef {ShortenPipe}
+ * @export
+ *
+ * @implements {PipeTransform}
+ */
 @Pipe({
 	name: 'shorten',
 })
 export class ShortenPipe implements PipeTransform {
-	/* Used to get the specific three letter code that will represent a word in a table
-      pulled from a JSON that I wrote. EX. Judgement -> Jud Priestess -> Prs 
-     @param toShorten: string to be shortened*/
+	/**
+	 * Used to get the specific three letter code that will represent a word in
+	 * a table pulled from a JSON that I wrote. EX. Judgement -> Jud Priestess
+	 * -> Prs
+	 *
+	 * @param {string} toShorten
+	 * @returns {string | undefined}
+	 */
 	transform(toShorten: string): string | undefined {
 		let shorten: { [long: string]: string } = {}
 		Object.entries(SHORTEN).forEach(([key, value]) => {
