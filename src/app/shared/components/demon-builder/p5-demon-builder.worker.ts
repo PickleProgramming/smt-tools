30f5cbd78bee8a6941b87d2decb8f985b4e59f53
PickleProgramming <matthew.e.pickle@gmail.com>
2024-05-19 22:55:36 -0400

    cleanup and document p5-demon-builder.worker

--- src/app/shared/components/demon-builder/p5-demon-builder.worker.ts
+++ src/app/shared/components/demon-builder/p5-demon-builder.worker.ts
@@ -1,38 +1,48 @@
 import { P5_COMPENDIUM, P5_FUSION_CALCULATOR } from '@shared/constants'
 import { DemonBuilder } from '@shared/types/demon-builder'
 import { BuildMessage, InputChainData } from '@shared/types/smt-tools.types'
-import {
-	Observable,
-	ReplaySubject,
-	Subject,
-	Subscriber,
-	from,
-	interval,
-	of,
-} from 'rxjs'
+import { Observable, ReplaySubject, from } from 'rxjs'
 import { P5Compendium } from '@p5/types/p5-compendium'
 import { P5FusionCalculator } from '@p5/types/p5-fusion-calculator'
 import _ from 'lodash'
 import { runWorker } from 'observable-webworker'
-import {
-	delay,
-	merge,
-	mergeMap,
-	repeat,
-	switchMap,
-	take,
-	takeUntil,
-} from 'rxjs/operators'
-import { ComplexOuterSubscriber } from 'rxjs/internal/innerSubscribe'
+import { delay, mergeMap } from 'rxjs/operators'
 import { BuildRecipe } from '@shared/types/build-recipe'
 
+/**
+ * Description placeholder
+ *
+ * @class P5DemonBuilderWorker
+ * @extends {DemonBuilder}
+ * @typedef {P5DemonBuilderWorker}
+ * @export
+ */
 export class P5DemonBuilderWorker extends DemonBuilder {
+	/**
+	 * @type {P5Compendium}
+	 * @inheritdoc {DemonBuilder.compendium}
+	 * @override
+	 * @see DemonBuilder.compendium
+	 */
 	declare compendium: P5Compendium
+	/**
+	 * @inheritdoc {DemonBuilder.calculator}
+	 * @see DemonBuilder.calculator
+	 */
 	declare calculator: P5FusionCalculator
 
+	/**
+	 * Creates an instance of P5DemonBuilderWorker.
+	 *
+	 * @class
+	 */
 	constructor() {
 		super(P5_COMPENDIUM, P5_FUSION_CALCULATOR)
 	}
+	/**
+	 * @inheritdoc {DemonBuilder.workUnit}
+	 * @see DemonBuilder.workUnit
+	 */
 	workUnit(input: InputChainData): Observable<BuildMessage> {
 		return this.getFusionChains(input)
 	}
@@ -42,6 +52,11 @@ export class P5DemonBuilderWorker extends DemonBuilder {
 	 * These methods will call the corresponding methods in the DEMON-NAME or
 	 * NO-NAME methods depending on if the user supplied a demonName or not.
 	 */
+
+	/**
+	 * @{inheritdoc DemonBuilder.getFusionChains}
+	 * @see DemonBuilder.getFusionChains
+	 */
 	protected getFusionChains(input: InputChainData): Observable<BuildMessage> {
 		if (input.maxLevel) this.maxLevel = input.maxLevel
 		if (input.recurDepth) this.recurDepth = input.recurDepth
@@ -49,10 +64,17 @@ export class P5DemonBuilderWorker extends DemonBuilder {
 			 calls, either with a specified demon or without.*/
 		this.isValid(input.targetSkills, input.demonName)
 		if (input.demonName) {
-			return this.demon_getFusionChains(input.targetSkills, input.demonName)
+			return this.demon_getFusionChains(
+				input.targetSkills,
+				input.demonName
+			)
 		}
 		return this.noDemon_getFusionChains(input.targetSkills)
 	}
+	/**
+	 * @{inheritdoc DemonBuilder.isValid}
+	 * @see DemonBuilder.isValid
+	 */
 	protected isValid(
 		targetSkills: string[],
 		demonName?: string | null
@@ -67,10 +89,15 @@ export class P5DemonBuilderWorker extends DemonBuilder {
 			}
 		}
 		if (demonName) {
-			return this.demon_isValidInput(targetSkills, demonName)
+			return this.demon_isValid(targetSkills, demonName)
 		}
-		return this.noDemon_isValidInput(targetSkills)
+		return this.noDemon_isValid(targetSkills)
 	}
+
+	/**
+	 * @{inheritdoc DemonBuilder.isPossible}
+	 * @see DemonBuilder.isPossible
+	 */
 	protected isPossible(targetSkills: string[], demonName?: string): boolean {
 		if (!this.checkSkillLevels(targetSkills)) return false
 		if (demonName) {
@@ -78,18 +105,10 @@ export class P5DemonBuilderWorker extends DemonBuilder {
 		}
 		return this.noDemon_isPossible(targetSkills)
 	}
+
 	/**
-	 * Checks if a demon can learn the number of skills in target skills. In P5,
-	 * normal demons can only inherit a maximum of 4 skills. Special demons can
-	 * inherit up to 5 under the right conditions. If the user specifies more
-	 * skills than a demon can inherit, the resultant demon will need to learn the
-	 * rest of the skills on their own.
-	 *
-	 * @param targetSkills List of skills for the resultant to learn
-	 * @param demonName Name of the resultant demon
-	 * @returns {possible: boolean, reason: string} If possible, reason is always
-	 *   null, if not possible, reason contains feedback for user about max
-	 *   inheritance.
+	 * @{inheritdoc DemonBuilder.canInheritOrLearn}
+	 * @see DemonBuilder.canInheritOrLearn
 	 */
 	private canInheritOrLearn(
 		targetSkills: string[],
@@ -100,12 +119,20 @@ export class P5DemonBuilderWorker extends DemonBuilder {
 		}
 		return this.noDemon_canInheritOrLearn(targetSkills)
 	}
+
 	/**
 	 * ---DEMON METHODS---
 	 *
 	 * These methods will be called when the user provides a demon name. All the
 	 * methods do the same thing as stated in DemonBuilder, just with different
 	 * arguments.
+	 */
+
+	/**
+	 * Overload of {@see DemonBuilder.getFusionChains} handling the operation
+	 * when a demon name is specified
+	 *
+	 * @overload
 	 */
 	protected demon_getFusionChains(
 		targetSkills: string[],
@@ -148,7 +175,13 @@ export class P5DemonBuilderWorker extends DemonBuilder {
 			sub.complete()
 		})
 	}
-	protected demon_isValidInput(
+	/**
+	 * Overload of {@see DemonBuilder.isValid} handling the operation when a
+	 * demon name is specified
+	 *
+	 * @overload
+	 */
+	protected demon_isValid(
 		targetSkills: string[],
 		demonName: string
 	): boolean {
@@ -174,6 +207,12 @@ export class P5DemonBuilderWorker extends DemonBuilder {
 		}
 		return true
 	}
+	/**
+	 * Overload of {@see DemonBuilder.isPossible} handling the operation when a
+	 * demon name is specified
+	 *
+	 * @overload
+	 */
 	protected demon_isPossible(
 		targetSkills: string[],
 		demonName: string
@@ -191,6 +230,12 @@ export class P5DemonBuilderWorker extends DemonBuilder {
 		}
 		return this.canInheritOrLearn(targetSkills, demonName)
 	}
+	/**
+	 * Overload of {@see DemonBuilder.canInheritOrLearn} handling the operation
+	 * when a demon name is specified
+	 *
+	 * @overload
+	 */
 	private demon_canInheritOrLearn(
 		targetSkills: string[],
 		demonName: string
@@ -224,6 +269,13 @@ export class P5DemonBuilderWorker extends DemonBuilder {
 	 * All the methods do the same thing as stated in DemonBuilder, just with
 	 * different arguments.
 	 */
+
+	/**
+	 * Overload of {@see DemonBuilder.getFusionChains} handling the operation
+	 * when NO demon name is specified
+	 *
+	 * @overload
+	 */
 	protected noDemon_getFusionChains(
 		targetSkills: string[]
 	): Observable<BuildMessage> {
@@ -253,12 +305,24 @@ export class P5DemonBuilderWorker extends DemonBuilder {
 			.subscribe(result)
 		return result
 	}
-	noDemon_isValidInput(targetSkills: string[]): boolean {
+	/**
+	 * Overload of {@see DemonBuilder.isValid} handling the operation when NO
+	 * demon name is specified
+	 *
+	 * @overload
+	 */
+	noDemon_isValid(targetSkills: string[]): boolean {
 		if (!this.canInheritOrLearn(targetSkills)) {
 			throw new Error(Errors.MaxSkills)
 		}
 		return true
 	}
+	/**
+	 * Overload of {@see DemonBuilder.isPossible} handling the operation when NO
+	 * demon name is specified
+	 *
+	 * @overload
+	 */
 	protected noDemon_isPossible(targetSkills: string[]): boolean {
 		//check if the skill is unique, if it is, fuse for that demon
 		let unique = this.hasUniqueSkills(targetSkills)
@@ -269,6 +333,12 @@ export class P5DemonBuilderWorker extends DemonBuilder {
 		}
 		return true
 	}
+	/**
+	 * Overload of {@see DemonBuilder.canInheritOrLearn} handling the operation
+	 * when NO demon name is specified
+	 *
+	 * @overload
+	 */
 	private noDemon_canInheritOrLearn(targetSkills: string[]): boolean {
 		//see if there are any demons with innates skills as such
 		for (let name in this.compendium.demons) {
@@ -284,6 +354,10 @@ export class P5DemonBuilderWorker extends DemonBuilder {
 	 *
 	 * These methods will work the same regardless of if the user provided demon
 	 * name was provided or not.
+	 */
+	/**
+	 * @{inheritdoc DemonBuilder.getFusionChain}
+	 * @see DemonBuilder.getFusionChain
 	 */
 	protected getFusionChain(
 		targetSkills: string[],
@@ -320,6 +394,7 @@ export class P5DemonBuilderWorker extends DemonBuilder {
 	/**
 	 * Makes sure all the skills passed are less than the max level
 	 *
+	 * @private
 	 * @param targetSkills Skills to check
 	 * @returns {possible: boolean, reason: string} True if all skills are less
 	 *   than the max level. If not, returns false and gives a reason
@@ -338,14 +413,18 @@ export class P5DemonBuilderWorker extends DemonBuilder {
 	/**
 	 * Gets a list of demons that have the skills specified
 	 *
+	 * @private
 	 * @param targetSkills Skills to look for
-	 * @returns List of demon names with those skills
+	 * @returns {string[]} List of demon names with those skills
 	 */
 	private getDemonsWithSkills(targetSkills: string[]): string[] {
 		let result: string[] = []
 		for (let demonName in this.compendium.demons) {
 			let demon = this.compendium.demons[demonName]
-			let innates = _.intersection(targetSkills, Object.keys(demon.skills))
+			let innates = _.intersection(
+				targetSkills,
+				Object.keys(demon.skills)
+			)
 			if (innates.length > 0) {
 				result.push(demonName)
 			}
@@ -356,9 +435,10 @@ export class P5DemonBuilderWorker extends DemonBuilder {
 	/**
 	 * Checks if there are any skills in the list are unique
 	 *
+	 * @private
 	 * @param targetSkills Skills to check
-	 * @returns The name of the demon that learns the skill if it is unique, if it
-	 *   is not unique returns an empty string
+	 * @returns {string} The name of the demon that learns the skill if it is
+	 *   unique, if it is not unique returns an empty string
 	 */
 	private hasUniqueSkills(targetSkills: string[]): string {
 		for (let skillName of targetSkills) {
@@ -369,7 +449,11 @@ export class P5DemonBuilderWorker extends DemonBuilder {
 	}
 }
 
-//enumeration to keep errors short within methods
+/**
+ * Enumeration to keep errors short within methods
+ *
+ * @enum {number}
+ */
 enum Errors {
 	LowLevel = 'The name of the demon you entered has a minimum level greater than the level you specified.',
 	Treasure = 'The name of the demon you entered is a treasure demon, and cannot be fused.',
@@ -377,5 +461,5 @@ enum Errors {
 	MaxSkills = 'In Persona 5, a normal demon can only inherit a maximum of 4 skills (special demons can inherit 5). Since you specificed more than that, your specified demon must be able to learn at least one of the other specificed skills on their own. Unfortunately, they cannot.',
 }
 
-/* we need this line to tell the worker to actually start. See observable-worker docmuentaiton for more details */
+//we need this line to tell the worker to actually start. See observable-worker docmuentaiton for more details
 runWorker(P5DemonBuilderWorker)
