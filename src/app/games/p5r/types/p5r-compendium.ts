25a382df35e0e3ab4dd9cb26e519948cae945348
PickleProgramming <matthew.e.pickle@gmail.com>
2024-06-05 13:01:22 -0400

    add traits field to p5r compendium

--- src/app/games/p5r/types/p5r-compendium.ts
+++ src/app/games/p5r/types/p5r-compendium.ts
@@ -1,10 +1,17 @@
 import _ from 'lodash'
 
-import { ElementTable, FusionTable } from '@shared/types/smt-tools.types'
+import { ElementTable, FusionTable, Skill } from '@shared/types/smt-tools.types'
 import { P5InheritanceType } from '@p5/types/p5-inheritance-types'
 import { P5Compendium } from '@p5/types/p5-compendium'
 
 export class P5RCompendium extends P5Compendium {
+	/**
+	 * Key/value pair of all the skill in the compendium with the type "trait"
+	 * represents all the traits a demon can possess in the game
+	 *
+	 * @type {{ [name: string]: Skill }}
+	 */
+	public declare traits: { [name: string]: Skill }
 	constructor(
 		personaData: Object,
 		skillData: Object,
@@ -23,5 +30,10 @@ export class P5RCompendium extends P5Compendium {
 			elementTable,
 			inheritData
 		)
+		// populate traits
+		for (let skillName of Object.keys(this.skills)) {
+			let skill = this.skills[skillName]
+			if (skill.element == 'trait') this.traits[skillName] = skill
+		}
 	}
 }
